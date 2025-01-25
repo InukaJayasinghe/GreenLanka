c7854c546b455be2a403c1f8bfb0646117b856d7
Inuka <inukajayasinghecampus@gmail.com>
2025-01-26 03:29:56 +0530

    maindart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,22 +2,21 @@ import 'package:flutter/foundation.dart';
 
 class AuthProvider with ChangeNotifier {
   bool _isAuthenticated = false;
-  String _userId = '';
+  String? _userId;
 
-  bool get _isAuthenticated => _isAuthenticated;
-  String get _userId => _userId;
+  bool get isAuthenticated => _isAuthenticated;
+  String? get userId => _userId;
 
   Future<void> signIn(String email, String password) async {
     // TODO: Implement actual authentication
     _isAuthenticated = true;
-    _userId = '12345';
+    _userId = 'user123';
     notifyListeners();
   }
-  
-  Future<vooid> signOut() async {
-    _isAuthenticated = false;
-    _userId = 'null';
-    notifyListeners();
 
+  Future<void> signOut() async {
+    _isAuthenticated = false;
+    _userId = null;
+    notifyListeners();
   }
-} 
+}
